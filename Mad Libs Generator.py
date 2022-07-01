d9dfb3e789c93977ae7066bdeca5e1e82268f085
beattiecolleen <108088943+beattiecolleen@users.noreply.github.com>
2022-07-01 14:02:31 +0200

    Add files via upload

---
+++ Mad Libs Generator.py
@@ -0,0 +1,31 @@
+#Mad Libs Generator
+
+
+#Input
+adjective_1 = input('Choose an adjective: ')
+noun_1 = input('Choose a noun: ')
+verb_past_tense_1 = input('Choose a verb in the past tense: ')
+adverb_1 = input('Choose an adverb: ')
+adjective_2 = input('Choose an adjective: ')
+noun_2 = input('Choose a noun: ')
+noun_3 = input('Choose a noun: ')
+adjective_4 = input('Choose an adjective: ')
+verb = input('Choose a verb in the present tense: ')
+adverb_2 = input('Choose an adverb: ')
+verb_past_tense_2 = input('Choose a verb in the past tense: ')
+adjective_5 = input('Choose an adjective: ')
+
+
+#Story
+print('------------------------------------------------------')
+print()
+
+print('Today I went to the zoo.')
+print('I saw a ', adjective_1, noun_1, ' jumping up and down in its tree.')
+print('It ', verb_past_tense_1, adverb_1, ' through the large tunnel that led to its ', adjective_2, noun_2,' .')
+print('I got some peanuts and passed them through the cage to a gigantic gray ',noun_3,' towering above my head.')
+print('Feeding that animal made me hungry.')
+print('I went to get a ',adjective_4, ' scoop of ice cream.')
+print('It filled my stomach.')
+print('Afterwards I had to ', verb, adverb_2, ' to catch our bus.')
+print('When I got home I ',verb_past_tense_2,' my mom for a ',adjective_5,' day at the zoo.')
